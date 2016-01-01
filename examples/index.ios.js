9ad251091fcf2596184fdb35e0ab2fc4dd4f7537
Christoph Jerolimov <jerolimov+git+2015@gmail.com>
2016-01-01 23:43:44 +0100

    Add example project.

---
+++ examples/index.ios.js
@@ -0,0 +1,13 @@
+
+import React, { AppRegistry, Component, View, Text } from 'react-native';
+
+import Markdown from 'react-native-showdown';
+
+class Example extends Component {
+    render() {
+        var markdown = '# Welcome to React Native!\n\nTo get started, edit index.ios.js\n\nShake or press menu button for dev menu';
+        return <Markdown body={ markdown } />
+    }
+}
+
+AppRegistry.registerComponent('examples', () => Example);
